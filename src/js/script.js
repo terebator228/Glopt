03a029bfcbb3fc837617a13276e36e0a2c8e3e8a
terebator228 <mish.8@mail.ru>
2023-01-12 19:07:19 +0300

    Верстка карусели отзывов

--- src/js/script.js
+++ src/js/script.js
@@ -46,4 +46,36 @@ $('form').submit(function(e) {
     return false;
 });
 
+const slider = tns({
+    container: '.reviews__inner',
+    items: 1,
+    slideBy: 'page',
+    autoplay: false,
+    controls: false,
+    nav: false,
+    responsive: {
+        640: {
+          edgePadding: 20,
+          gutter: 20,
+          items: 1
+        },
+        700: {
+          gutter: 30
+        },
+        900: {
+          items: 1
+        }
+      }
+});
+
+
+
+document.querySelector('.prev').addEventListener('click',function () {
+    slider.goTo('prev');
+});
+
+document.querySelector('.next').addEventListener('click',function () {
+    slider.goTo('next');
+});
+
 });
